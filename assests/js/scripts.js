f4063948304d58b930d173bb974c32a2722859ea
lakshan <lakshanrashmika16302@gmail.com>
2024-07-10 16:12:54 +0530

    updated

--- assests/js/scripts.js
+++ assests/js/scripts.js
@@ -877,3 +877,57 @@ $(document).ready(function () {
     initRestabook();
     initparallax();
 });
+
+// Brand Active
+$('.brand-active').slick({
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    speed: 400,
+    arrows: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: false,
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 780,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 991,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        },
+
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
